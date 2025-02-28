ab039353654b21844e062437e873f0d9ea009cad
apate302 <apate302@charlotte.edu>
2025-02-28 02:19:37 -0500

    Integrating Custom User Model

--- studybud/settings.py
+++ studybud/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
 
 ]
 
+AUTH_USER_MODEL = 'base.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -124,10 +126,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIAL_URL = '/images/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
 
 #STATIC_ROOT = 
 
